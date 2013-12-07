92b337a0479469eaeeb044b4301f309256a9220c
RememberLenny <leonard_bogdonoff@condenast.com>
2013-12-07 16:47:01 -0500

    Saving the youtube frame

--- public/js/app.js
+++ public/js/app.js
@@ -19,6 +19,8 @@
         setTimeout(function(){
           $($('.main-container h2')[2]).removeClass('hidden');
 
+            setTimeout(function(){
+              $('.main-container h3').removeClass('hidden');
                 // H1
                 setTimeout(function(){
                   $('.main-container h1').removeClass('hidden');
@@ -27,39 +29,64 @@
                   setTimeout(function(){
                     $('.main-container .button').removeClass('hidden');
                     setTimeout(function(){
-                      $('.footer, .top-bar-container').css('opacity', '100');
+                      $('.footer, .top-bar-container, .tab-bar').css('opacity', '100');
                     }, 500);
                   }, 500);
-                }, 1000);
+                }, 500);
+            }, 600);
 
         }, 300);
-      }, 400);
-    }, 500);
+      }, 300);
+    }, 300);
+
+    var append = '<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/lBBibhvmoCw?autoplay=1&controls=0&enablejsapi=1&modestbranding=1&rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>';
+    var playing = false;
+    $(document).on('click', '#play-video', function(){
+      if( playing === false ){
+        playing = true;
+        setTimeout(function(){
+          $('.video-player-contain').append(append);
+        }, 250);
+      }
+    });
+    $(document).on('close', '[data-reveal]', function () {
+      playing = false;
+      setTimeout(function(){
+        $('.video-player-contain').html(' ');
+      }, 250);
+    });
+
   });
+
   (function checkPosition(){
     window.setTimeout(function(){
       var windowHeight = $(window).height();
+      var windowWidth = $(window).width();
       var footerHeight = $('.footer').height();
       var topbarHeight = $('.top-bar-container').height();
       var maincontainHeight = $('.main-container').height();
-      var combinedContainers = footerHeight+topbarHeight+maincontainHeight;
+      var combinedContainers = topbarHeight+maincontainHeight;
       var contentArea = windowHeight-combinedContainers;
-      var contentTopMargin = contentArea/2.5;
+      if( windowWidth > windowHeight){
+        var contentTopMargin = contentArea*.6;
+      } else {
+        var contentTopMargin = contentArea *.3;
+      }
       $('.content-main').css('margin-top', contentTopMargin);
       checkPosition();
 
       var actModalHeight = $('#myModal').height();
-      var modalSpacing = (windowHeight - actModalHeight)/3;
+      var modalSpacing = (windowHeight - actModalHeight)/2;
       var modalHeight = windowHeight - 200;
       var modalWidth = $('#myModal').width();
-      var modalDisplace = -((modalWidth/2) + 30);
+      var modalDisplace = -((modalWidth/2));
       $('object embed').css('height', modalHeight);
       $('.reveal-modal').css('margin-left', modalDisplace);
       $('#myModal.open').css('top', modalSpacing);
-
+      $('.inner-wrap').height(windowHeight);
     }, 250);
 
-    $('a').on('click',function(){
+    $('a.fadeout').on('click',function(){
       $('body').fadeOut();
       setTimeout(300);
     });
